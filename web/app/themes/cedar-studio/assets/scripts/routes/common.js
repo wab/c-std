5c79269866e11fe520d7e45b0192bee0cbed60a9
wabdsgn <wab2com@gmail.com>
2017-04-07 01:06:29 +0200

    navigation mobile

--- web/app/themes/cedar-studio/assets/scripts/routes/common.js
+++ web/app/themes/cedar-studio/assets/scripts/routes/common.js
@@ -1,8 +1,43 @@
+/* global */
 export default {
   init() {
     // JavaScript to be fired on all pages
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
+    $(document).foundation();
+
+    // navigation mobile
+    $('.menu-link').click(() => {
+      $('.navigation-menu').toggleClass('opened');
+    });
+
+    // Scroll animation speed
+    const speed = 200;
+    // Cashing objects
+    const $root = $('html, body');
+    const $selector = $('a[href*="#"]:not([href="#"])');
+
+    $selector.on('click', function scroll(event) {
+      if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') &&
+          location.hostname === this.hostname) {
+        // Prevent default link behavior
+        event.preventDefault();
+
+        // Store target object
+        let target = $(this.hash);
+        target = target.length ? target : $(`[name='${target.slice(1)}`);
+
+        // Animate scroll
+        if (target.length) {
+          $root.animate({
+            scrollTop: target.offset().top,
+          }, speed, () => {
+            // Update URL witch correct hash
+            window.location.hash = target.selector;
+          });
+        }
+      }
+    });
   },
 };
