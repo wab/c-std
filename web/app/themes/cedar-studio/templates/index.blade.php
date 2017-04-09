3d3a445d59e91de92c34650fbf2b6343a59f18d8
wabdsgn <wab2com@gmail.com>
2017-04-09 22:05:49 +0200

    blog page

--- web/app/themes/cedar-studio/templates/index.blade.php
+++ web/app/themes/cedar-studio/templates/index.blade.php
@@ -2,17 +2,29 @@
 
 @section('content')
   @include('partials.page-header')
+  <div class="section">
+    <div class="row">
+      <div class="columns medium-8">
+        @if (!have_posts())
+          <div class="alert alert-warning">
+            {{ __('Sorry, no results were found.', 'sage') }}
+          </div>
+          {!! get_search_form(false) !!}
+        @endif
 
-  @if (!have_posts())
-    <div class="alert alert-warning">
-      {{ __('Sorry, no results were found.', 'sage') }}
+        @while (have_posts()) @php(the_post())
+          @include ('partials.content-'.(get_post_type() !== 'post' ? get_post_type() : get_post_format()))
+        @endwhile
+
+        {!! get_the_posts_navigation() !!}
+
+      </div>
+      <div class="columns medium-4">
+        <aside class="sidebar">
+          @include('partials.sidebar')
+        </aside>
+      </div>
     </div>
-    {!! get_search_form(false) !!}
-  @endif
+  </div>
 
-  @while (have_posts()) @php(the_post())
-    @include ('partials.content-'.(get_post_type() !== 'post' ? get_post_type() : get_post_format()))
-  @endwhile
-
-  {!! get_the_posts_navigation() !!}
 @endsection
