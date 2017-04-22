b4f07af6a133369bd81911c65f1355c099c7eb1c
wabdsgn <wab2com@gmail.com>
2017-04-23 01:38:14 +0200

    sections, breadcrumb & font

---
+++ web/app/themes/cedar-studio/templates/partials/breadcrumb.blade.php
@@ -0,0 +1,7 @@
+<div class="breadcrumbs" typeof="BreadcrumbList" vocab="http://schema.org/">
+  @if(function_exists('bcn_display'))
+  <div class="breadcrumbs-container">
+    @php(bcn_display())
+  </div>
+  @endif
+</div>
--- web/app/themes/cedar-studio/templates/partials/page-header.blade.php
+++ web/app/themes/cedar-studio/templates/partials/page-header.blade.php
@@ -1,37 +1,22 @@
-<?php if (is_front_page()): ?>
+@if (is_front_page())
   <header class="page-header text-center banner">
     <div class="wrapper">
       <h1 class="maintitle page--title">@php(bloginfo('title'))<span class="tagline">@php(bloginfo('description'))</span></h1>
       <a href="#" class="button large focus">Commander mon projet</a>
     </div>
   </header>
-
-<?php elseif (is_single() ): ?>
+@elseif (is_single() )
    <div class="page-header text-center banner">
     <div class="wrapper">
       <p class="page--title">Blog</p>
     </div>
   </div>
-  <div class="breadcrumbs" typeof="BreadcrumbList" vocab="http://schema.org/">
-    <?php if(function_exists('bcn_display')):?>
-    <div class="row column">
-      <?php bcn_display(); ?>
-    </div>
-    <?php endif; ?>
-  </div>
-<?php else:  ?>
+@else
   <header class="page-header text-center banner">
     <div class="wrapper">
       <h1 class="page--title"><?= App\title(); ?></h1>
     </div>
   </header>
-  <?php if (!is_page_template('templates/page-iframe.php')): ?>
-    <div class="breadcrumbs" typeof="BreadcrumbList" vocab="http://schema.org/">
-      <?php if(function_exists('bcn_display')):?>
-      <div class="row column">
-        <?php bcn_display(); ?>
-      </div>
-      <?php endif; ?>
-    </div>
-  <?php endif; ?>
-<?php endif; ?>
+@endif
+
+@include('partials.breadcrumb')
