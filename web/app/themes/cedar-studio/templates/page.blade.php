67c8a7cfee1878e4b6fcb2e6e5acdcbe1c36cb1b
wabdsgn <wab2com@gmail.com>
2017-04-28 23:55:48 +0200

    correction page

--- web/app/themes/cedar-studio/templates/page.blade.php
+++ web/app/themes/cedar-studio/templates/page.blade.php
@@ -3,14 +3,6 @@
 @section('content')
   @while(have_posts()) @php(the_post())
     @include('partials.page-header')
-    <div class="section">
-      <div class="row">
-        <div class="columns large-6 medium-6 medium-centered">
-          @include('partials.content-page')
-        </div>
-      </div>
-    </div>
-
-    @include('partials.section')
+    @include('partials.content-page')
   @endwhile
 @endsection
