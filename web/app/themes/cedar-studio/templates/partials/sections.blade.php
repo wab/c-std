900a4b854f58c475f05f91bbc1617c2b4be68394
wabdsgn <wab2com@gmail.com>
2017-05-29 22:40:52 +0200

    modification sur les sections

--- web/app/themes/cedar-studio/templates/partials/sections.blade.php
+++ web/app/themes/cedar-studio/templates/partials/sections.blade.php
@@ -3,23 +3,26 @@
   @while ( have_rows('section') ) @php(the_row())
   @php($image = get_sub_field('image'))
   <section class="section">
+    <h2 class="section-title"><span>@php(the_sub_field('title'))</span></h2>
     <div class="section-wrapper">
         @if(!empty($image))
-          @php
-            // variables
-            $url = $image['url'];
-            $title = $image['title'];
-            $alt = $image['alt'];
-            $caption = $image['caption'];
+          <div class="section-thumbnail">
+            @php
+              // variables
+              $url = $image['url'];
+              $title = $image['title'];
+              $alt = $image['alt'];
+              $caption = $image['caption'];
 
-            // thumbnail
-            $size = 'large';
-            $thumb = $image['sizes'][ $size ];
-          @endphp
-          <img src="{{$thumb}}" alt="{{$alt}}" class="section-thumbnail">
+              // thumbnail
+              $size = 'large';
+              $thumb = $image['sizes'][ $size ];
+            @endphp
+            <img src="{{$thumb}}" alt="{{$alt}}">
+          </div>
         @endif
-        <h2 class="page-section-title"><span>@php(the_sub_field('title'))</span></h2>
-        <div class="page-section-contenu">
+        <div class="section-contenu">
+
           @php(the_sub_field('content'))
         </div>
     </div>
